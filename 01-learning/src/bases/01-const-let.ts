98ce0039f47484c64f981426bb6b193ba59d821e
Rus Madrigal <rusbenmadrigal@gmail.com>
2026-01-19 10:49:10 -0600

    feat: add 01-const-let.ts for demonstrating const and let usage

---
+++ 01-learning/src/bases/01-const-let.ts
@@ -0,0 +1,12 @@
+
+const firstName = 'Rusben';
+const lastName = 'Madrigal';
+
+
+let diceNumber = 5;
+diceNumber = 3;
+
+const constainsLetterM = lastName.includes('M');
+
+console.log({ constainsLetterM, diceNumber })
+

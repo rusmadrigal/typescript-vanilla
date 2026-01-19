4dc92d22b05cfaae92ab856f02598e98de71193f
Rus Madrigal <rusbenmadrigal@gmail.com>
2026-01-19 10:10:07 -0600

    refactor: clean up main.ts by removing unnecessary lines and add new 01-const-let.ts file

--- 01-learning/src/main.ts
+++ 01-learning/src/main.ts
@@ -1,12 +1,7 @@
 import './style.css'
 
-
-
-
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
   <H1> Hello World</H1>
   </div>
 `
-
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)

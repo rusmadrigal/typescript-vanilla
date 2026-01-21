f7f2e9e8b445d9398d232520e96f3f8e55a7e5c4
Rus Madrigal <rusbenmadrigal@gmail.com>
2026-01-20 21:26:19 -0600

    Test

--- 01-learning/src/bases/01-const-let.ts
+++ 01-learning/src/bases/01-const-let.ts
@@ -8,5 +8,5 @@ diceNumber = 3;
 
 const constainsLetterM = lastName.includes('M');
 
-console.log({ constainsLetterM, diceNumber })
+console.log({ constainsLetterM, diceNumber, firstName, lastName });
 
